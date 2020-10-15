01e58d3e024af94fd4b21367570293a00cc0f91a
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-10-15 16:11:01 -0500

    Task 1 complete

--- index.js
+++ index.js
@@ -27,11 +27,14 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ *    declaring counter1 holding a function where inside the function we declare a count variable, returning count increamented by 1 when the function is called, but everytime the function is called
+ *    count variable resets to 0, counter2 is decalaring a function returning count variable incremented by 1 but does not reset the variable since it is outside of its scope.
  * 
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ *    counter1 is using closure since we are returning a callback function within counterMaker function. 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ *    Counter1 would work more when you are trying to get a single repeated answer everytime the function is called, counter2 increments the variable by one everytime it is called.
+ * 
 */
 
 // counter1 code
